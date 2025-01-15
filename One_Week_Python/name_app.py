523e898299a11b95cf024817669738277ad925fe
bryan <novobura@gmail.com>
2025-01-16 06:13:53 +0900

    Add files via upload

---
+++ One_Week_Python/name_app.py
@@ -0,0 +1,6 @@
+print("***WELCOME TO THE NAME APP***")
+first_name = input("What is your name??")
+last_name = input("What is your last name??")
+
+full_name = first_name + " " + last_name
+print("Hi there, " + full_name +"!")
