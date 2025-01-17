6d4562b908fab06e9d29b497a3d9683373a9b930
bryan <novobura@gmail.com>
2025-01-18 03:30:05 +0900

    Update name_length.py

--- One_Week_Python/name_length.py
+++ One_Week_Python/name_length.py
@@ -3,9 +3,11 @@ last_name = input("What is your last name: ")
 
 full_len = len(first_name) + len(last_name)
 
+print("*"*20)
 if full_len > 12:
     print(f"{first_name} {last_name} is longer than average length of an American name.")
 elif full_len == 12:
     print(f"{first_name} {last_name} is equal to the average length of an American name.")
 else:
     print(f"{first_name} {last_name} is shorter than average length of an American name.")
+print("*"*20)
