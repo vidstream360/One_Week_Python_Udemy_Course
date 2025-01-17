1da96b7d714fb55e450728332e28baa6bdf6cd8a
bryan <novobura@gmail.com>
2025-01-18 04:50:32 +0900

    Add files via upload

---
+++ One_Week_Python/temp_test1.py
@@ -0,0 +1,11 @@
+temp = 32
+#unit = 'fahrenheit'
+unit = 'celcius'
+
+if unit == 'fahrenheit':
+    if temp <= 32:
+        print("It's not freezing out!")
+    else:
+        print("It's not freezing ")
+else:
+    print("Im really bad with celcius!")
