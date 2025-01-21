dfcd7a8579143f9ef651c85c462bd82c291a25d1
bryan <novobura@gmail.com>
2025-01-22 07:58:21 +0900

    Add files via upload

---
+++ One_Week_Python/while_count.py
@@ -0,0 +1,25 @@
+
+num = 10
+
+while num > 0:
+    print(f"Number is {num}")
+    num -= 1
+
+num = 1
+
+while num <= 10:
+    print(f"Number is {num}")
+    num += 1
+
+count = 1
+while count < 8:
+    print("*" * count)
+    count += 1
+
+count = 7
+while count >0:
+    print("*" * count)
+    count -= 1
+
+
+
