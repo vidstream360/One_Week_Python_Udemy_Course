7da5d7236c4f81107e00c027bd05ea9ad766e082
bryan <novobura@gmail.com>
2025-01-22 05:26:09 +0900

    Add files via upload

---
+++ One_Week_Python/movie_theater_combo.py
@@ -0,0 +1,12 @@
+#not comes before and
+#and comes before or
+
+#veterans get in free on tuesdays
+# infants under 2 get in free always
+
+day = "Tuesday"
+is_vet = True
+age = 56
+
+if age <= 2 or is_vet and day == "Tuesday":
+    print("You get in free today!")
