e8c203298e2aacdf6124545080e4114990f357ee
bryan <novobura@gmail.com>
2025-01-24 11:49:52 +0900

    Add files via upload

---
+++ One_Week_Python/range.py
@@ -0,0 +1,20 @@
+#range examples
+
+for num in range(-10,10):
+    print(num)
+
+for num in range(10):
+    print("Hello World!")
+
+for num in range(0,22,2):
+    print(num)
+
+for num in range(10,1,-1):
+    print(num)
+
+#compare for loop range to while count with variable, for loop is cleaner
+
+count = 10
+while count >1:
+    print(count)
+    count -= 1
