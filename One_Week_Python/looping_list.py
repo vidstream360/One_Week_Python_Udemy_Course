7968b678d55386bd8ef51b65fc5bb02e14c84843
bryan <novobura@gmail.com>
2025-01-31 14:56:07 +0900

    Add files via upload

---
+++ One_Week_Python/looping_list.py
@@ -0,0 +1,10 @@
+emails = ['jimbo@gmail.com', 'daisy@yahoo.com', 'ds328@princess.net', 'anika.345@gmail.com', 'pretyinpink@pink.com', 'otter@facebook.com', 'elton@disney.com', 'jessica@gmail.com', 'wang3@gmail.com']
+
+for email in emails:
+    print("****SENDING EMAIL TO****")
+    print(email)
+
+idx = 0
+while idx < len(emails):
+    print(emails[idx])
+    idx +=1
