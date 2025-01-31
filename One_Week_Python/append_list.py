077dc30e2d1a3d9fd2a3793d50cbdbfa8721fe4f
bryan <novobura@gmail.com>
2025-01-31 09:10:31 +0900

    Add files via upload

---
+++ One_Week_Python/append_list.py
@@ -0,0 +1,14 @@
+
+langs = ["Python", "C", "javascript"]
+
+print(langs[1])
+
+print(langs[0])
+
+waitlist = ['juan', 'aria', 'amir']
+
+print(waitlist)
+
+waitlist.append("rosa")
+
+print(waitlist)
