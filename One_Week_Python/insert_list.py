ea29e3fd4fa42c2ba74cf07c61287d6034e6654f
bryan <novobura@gmail.com>
2025-01-31 09:14:54 +0900

    Add files via upload

---
+++ One_Week_Python/insert_list.py
@@ -0,0 +1,15 @@
+nums = [7,3,9]
+print(nums)
+
+nums.insert(1,"hi")
+print(nums)
+
+waitlist = ['juan', 'aria', 'amir']
+
+print(waitlist)
+
+waitlist.insert(0, "jasmine")
+
+waitlist.insert(3, "uma")
+
+print(waitlist)
