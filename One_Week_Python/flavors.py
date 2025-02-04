c0f3a8293c170dfd95124bd70941c119ba26e10d
bryan <novobura@gmail.com>
2025-02-04 17:53:42 +0900

    Add files via upload

---
+++ One_Week_Python/flavors.py
@@ -0,0 +1,10 @@
+#common pattern of check in a variable is in or not in a list
+
+flavors = ["chocolate", "vanilla", "lemon", "strawberry"]
+
+response = input("What flavor would you like?")
+
+while response.lower() not in flavors:
+    response = input("Flavor not found")
+
+print(f{"Ok {response} ice cream coming right up!"}) 
