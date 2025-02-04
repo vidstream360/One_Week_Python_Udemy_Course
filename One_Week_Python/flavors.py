4c033233d6573ff11c8302e6089766733a2546ed
bryan <novobura@gmail.com>
2025-02-04 17:56:23 +0900

    Update flavors.py

--- One_Week_Python/flavors.py
+++ One_Week_Python/flavors.py
@@ -5,6 +5,6 @@ flavors = ["chocolate", "vanilla", "lemon", "strawberry"]
 response = input("What flavor would you like?")
 
 while response.lower() not in flavors:
-    response = input("Flavor not found")
+    response = input("Flavor not available. Please choose another flavor")
 
-print(f{"Ok {response} ice cream coming right up!"}) 
+print(f"Ok {response} ice cream coming right up!") 
