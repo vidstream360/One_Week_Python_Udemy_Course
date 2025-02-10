32faf896334f83fd79151b80cfc59c3283c0bed4
bryan <novobura@gmail.com>
2025-02-10 11:41:47 +0900

    Add files via upload

---
+++ One_Week_Python/farmstand_nested_dict.py
@@ -0,0 +1,25 @@
+#example of a nested list
+
+prices = {
+    "arugala" : 1.15,
+    "basil" : 2.54,
+    "blackberries" : 4.93,
+    "blueberries" : 2.88,
+    "coconut" : 7.15,
+    "fennel" : 3.36,
+}
+
+produce = {
+    "arugala": {
+        "price": 1.15,
+        "qty": 61,
+        "organic": True,
+        "producer": "Blue Kitty Farms"
+    },
+    "coconut": {
+        "price": 7.15,
+        "qty": 12,
+        "organic": False,
+        "producer": "Tropical Dream Farm"
+    }
+}
