62a72ee0aabb32d6d5a176e95bcc8fe7989dbdc2
bryan <novobura@gmail.com>
2025-02-24 05:44:49 +0900

    Add files via upload

---
+++ OOP/dog.py
@@ -0,0 +1,36 @@
+#a class is a blueprint for objects that says what objects have in terms of attributes + functionality
+#attributes are variables / data 
+#methods add functionality
+#first parameter for def is self
+#first will refer to an instance of the class
+
+class Dog:
+    def __init__(self, name, breed, location):
+        self.name = name
+        self.breed = breed
+        self.location = location
+        self.tricks = []
+
+    def bark(self):
+        print(f"{self.name} says WOOF!")
+
+    def eat(self):
+        print("NOM NOM!")
+
+    def learn_trick(self,new_trick):
+        if new_trick not in self.tricks:
+            self.tricks.append(new_trick)
+
+    def perfrom_trick(self, trick):     
+        if trick in self.tricks:
+            print(f"{self.name} performs {trick}")
+        else:
+            print(f"{self.name} does not know {trick}")
+
+elton = Dog('elton', 'australian sheperd', 97236)
+elton.learn_trick('sit')
+elton.learn_trick('stay')
+
+jimbo = Dog('jimbo', 'mutt', 97236)
+jimbo.learn_trick('sit')
+jimbo.learn_trick('stay')
