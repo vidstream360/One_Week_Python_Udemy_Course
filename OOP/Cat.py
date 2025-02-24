742695576c87855b381bcf75bc6792d95b7edb27
bryan <novobura@gmail.com>
2025-02-24 17:07:36 +0900

    Add files via upload

---
+++ OOP/Cat.py
@@ -0,0 +1,31 @@
+#inheritance
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+
+    def meow(self):
+        print(f"{self.name} meows!!!!")
+
+
+c = Cat('blue')
+print(c.name)
+print(c.meow())
+
+class Cougar(Cat):               #class cougar inherits its attributes from class cat this way
+    def purr(self):              #if no init method it calls it from mother class
+        print(f"{self.name} purrrs")
+
+puma = Cougar('tina')
+
+print(puma.name)
+print(puma.purr())
+
+class Lion(Cat):
+    def roar(self):
+        print(f"{self.name} roars")
+
+lion = Lion('eli')
+
+print(lion.name)
+print(lion.roar())
