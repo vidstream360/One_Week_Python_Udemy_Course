34e136abe0d257ff5b0cef0483667383af33bd29
bryan <novobura@gmail.com>
2025-02-24 17:15:39 +0900

    Update Cat.py

--- OOP/Cat.py
+++ OOP/Cat.py
@@ -29,3 +29,11 @@ lion = Lion('eli')
 
 print(lion.name)
 print(lion.roar())
+
+class Tiger(Cat):
+    def __init__(self, name, pride_name):
+        print("INSIDE TIGER INIT")
+        super().__init__(name)
+        self.pride_name = pride_name
+    def roar(self):
+        print(f"{self.name} roars")
